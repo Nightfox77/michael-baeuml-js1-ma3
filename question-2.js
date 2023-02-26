2f9b4c7fb4246b1eab3278913ffb1ee5d45716b7
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2023-02-26 11:42:08 +0100

    1

--- question-2.js
+++ question-2.js
@@ -1,7 +1,7 @@
 const key = `efdefe3f99f047c9934528e800ad4187`;
 const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`;
 const container = document.querySelector(".container");
-container.innerHTML = "Loading...";
+
 
 async function getList() {
     try {
